99ccf1c76c79562b5df1b8b37105d5ea061b57e5
Deekshith422003 <drangineni@unomaha.edu>
2025-12-08 09:54:52 -0600

    made changes in UI

--- workouts/urls.py
+++ workouts/urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import WorkoutViewSet, ExerciseViewSet
 
 router = DefaultRouter()
-router.register(r'workouts', WorkoutViewSet)
-router.register(r'exercises', ExerciseViewSet)
+router.register("workouts", WorkoutViewSet, basename="workouts")
+router.register("exercises", ExerciseViewSet, basename="exercises")
+
 urlpatterns = router.urls
